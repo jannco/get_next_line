ffc1e69e7b7106fb5018cdb6d277d9aecb5b362f
yadereve <wikifixcom@gmail.com>
2023-11-09 19:52:19 +0000

    criar get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yadereve <yadereve@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 19:00:15 by yadereve          #+#    #+#             */
+/*   Updated: 2023/11/09 19:46:58 by yadereve         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 42
+
+# include <unistd.h>
+# include <stdint.h>
+
+#endif
