a866e7d97accd85ed5bcf733a806d2b791a88979
yadereve <wikifixcom@gmail.com>
2023-11-13 17:23:43 +0000

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yadereve <yadereve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:59:46 by yadereve          #+#    #+#             */
-/*   Updated: 2023/11/09 20:15:44 by yadereve         ###   ########.fr       */
+/*   Updated: 2023/11/13 16:21:22 by yadereve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,22 @@ char	*get_next_line(int fd)
 
 int	main()
 {
-	int	fd;
+	int		fd;
+	char	*line;
 
-	fd = open("test.txt", O_RDWR);
+	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("file opening filed");
+		return (1);
+	}
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		line = get_next_line(fd);
+		printf("line: %s\n", line);
+		free(line);
+	}
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,20 @@
 /*   By: yadereve <yadereve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 19:00:15 by yadereve          #+#    #+#             */
-/*   Updated: 2023/11/09 20:15:01 by yadereve         ###   ########.fr       */
+/*   Updated: 2023/11/13 16:00:14 by yadereve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
 
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+
+# ifdef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 #endif
